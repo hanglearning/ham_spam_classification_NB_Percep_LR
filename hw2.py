d659e1e21ee560c90e57c261281aa84f9743eeff
Hang Chen <hangforjobs@gmail.com>
2019-04-04 04:48:37 -0400

    why always 103/140 (74%)

--- hw2.py
+++ hw2.py
@@ -248,9 +248,9 @@ def classify_and_test_for_accuracy(classifier, test_on_validation=None, perceptr
 
 # Choosing the best lambda for logistic regression
 print("\nChoosing the best regularization constant lambda for Logistic Regression...")
-print("We will use 20 training iterations and the learning rate 0.003, and test for lambda in [0, 10]")
-learning_rate = 0.003
-training_iterations = 20
+learning_rate = 0.1
+training_iterations = 5
+print("We will use {} training iterations and the learning rate {}, and test for lambda in [0, 10]".format(training_iterations, learning_rate))
 regularization_lambda_with_accuracy = {}
 for regularization_lambda in range(11):
     print("Using lambda {} on the 70% training set...".format(regularization_lambda))
